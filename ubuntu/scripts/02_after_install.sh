7b9c6d29a02360f0e1b248a59db060e342504d5c
cesarbrunoms <cesarbrunoms@gmail.com>
2024-01-06 19:11:40 -0400

    update after install

--- ubuntu/scripts/02_after_install.sh
+++ ubuntu/scripts/02_after_install.sh
@@ -72,17 +72,7 @@ while true; do
       ;;
     6)
       # Opção 2: Instalar Google Chrome
-      # Baixar o pacote do Google Chrome
-      wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable.deb
-
-      # Instalar o Google Chrome
-      sudo dpkg -i google-chrome-stable.deb
-      sudo apt install -f
-
-      # Remover o pacote baixado (opcional)
-      rm google-chrome-stable.deb
-
-      echo "GOOGLE CHROME INSTALADO COM SUCESSO!"
+      bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_google_chrome.sh -O -)"
       ;;
     7)
       # Opção 3: Sair do script
