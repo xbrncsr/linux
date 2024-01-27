7de160d86926a7d9e2178fdaba631dda764aacb0
xbrncsr <cesarbrunoms@gmail.com>
2024-01-27 11:08:03 -0400

    1

--- ubuntu/scripts/teste.sh
+++ ubuntu/scripts/teste.sh
@@ -32,6 +32,19 @@ mostrar_menu() {
   fi
 }
 
+# Função para instalar e verificar a conclusão
+instalar_opcao() {
+  local numero_opcao=$1
+  local url=$2
+  local nome_opcao=$3
+
+  if bash -c "$(wget $url -O -)" 2>&1; then
+    opcoes_concluidas+=("$numero_opcao - $nome_opcao")
+  else
+    opcoes_nao_concluidas+=("$numero_opcao - $nome_opcao")
+  fi
+}
+
 # Loop principal
 while true; do
   mostrar_menu
@@ -42,92 +55,49 @@ while true; do
   case $choice in
     1)
       # Opção 1: Atualizar o sistema
-      if sudo apt update 2>&1; then
-        opcoes_concluidas+=("1")
-      else
-        opcoes_nao_concluidas+=("1")
-      fi
+      instalar_opcao "1" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/update_system.sh" "Atualizar o sistema"
       ;;
     2)
       # Opção 2: Instalar Ubuntu Restricted Extras
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ubuntu_restricted_extras.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("2")
-      else
-        opcoes_nao_concluidas+=("2")
-      fi
+      instalar_opcao "2" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ubuntu_restricted_extras.sh" "Instalar Ubuntu Restricted Extras"
       ;;
     3)
       # Opção 3: Instalar Fontes Microsoft, Htop, IPcalc, Gparted, Neofetch, FFmpeg e Habilitar x86
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_fonts_microsoft.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("3")
-      else
-        opcoes_nao_concluidas+=("3")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_htop.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("Htop")
-      else
-        opcoes_nao_concluidas+=("Htop")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ipcalc.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("IPCalc")
-      else
-        opcoes_nao_concluidas+=("IPCalc")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_gparted.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("Gparted")
-      else
-        opcoes_nao_concluidas+=("Gparted")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_neofetch.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("Neofetch")
-      else
-        opcoes_nao_concluidas+=("Neofetch")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ffmpeg.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("FFmpeg")
-      else
-        opcoes_nao_concluidas+=("FFmpeg")
-      fi
-
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/enable_x86.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("Habilitar x86")
-      else
-        opcoes_nao_concluidas+=("Habilitar x86")
-      fi
+      instalar_opcao "3.1" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_fonts_microsoft.sh" "Instalar Fontes Microsoft"
+      instalar_opcao "3.2" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_htop.sh" "Instalar Htop"
+      instalar_opcao "3.3" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ipcalc.sh" "Instalar IPCalc"
+      instalar_opcao "3.4" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_gparted.sh" "Instalar Gparted"
+      instalar_opcao "3.5" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_neofetch.sh" "Instalar Neofetch"
+      instalar_opcao "3.6" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_ffmpeg.sh" "Instalar FFmpeg"
+      instalar_opcao "3.7" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/enable_x86.sh" "Habilitar x86"
       ;;
     4)
       # Opção 4: Instalar Git
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_git.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("4")
-      else
-        opcoes_nao_concluidas+=("4")
-      fi
+      instalar_opcao "4" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_git.sh" "Instalar Git"
       ;;
     5)
       # Opção 5: Instalar Virt-Manager
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_virt_manager.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("5")
-      else
-        opcoes_nao_concluidas+=("5")
-      fi
+      instalar_opcao "5" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_virt_manager.sh" "Instalar Virt-Manager"
       ;;
     6)
       # Opção 6: Instalar VirtualBox
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_virtualbox.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("6")
-      else
-        opcoes_nao_concluidas+=("6")
-      fi
+      instalar_opcao "6" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_virtualbox.sh" "Instalar VirtualBox"
       ;;
     7)
       # Opção 7: Instalar Google Chrome
-      if bash -c "$(wget https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_google_chrome.sh -O -)" 2>&1; then
-        opcoes_concluidas+=("7")
-      else
-        opcoes_nao_concluidas+=("7")
-     
+      instalar_opcao "7" "https://raw.githubusercontent.com/cesarbrunoms/linux/main/ubuntu/scripts/script_files/install_google_chrome.sh" "Instalar Google Chrome"
+      ;;
+    8)
+      # Opção 8: Sair do script
+      break
+      ;;
+    *)
+      # Opção inválida
+      echo "=====================> [ OPÇÃO INVÁLIDA, TENTE NOVAMENTE! ]"
+      ;;
+  esac
+
+  # Pausa para o usuário ler a mensagem antes de limpar a tela
+  read -p "=====================> [ PRESSIONE ENTER PARA CONTINUAR...! ]"
+
+done
